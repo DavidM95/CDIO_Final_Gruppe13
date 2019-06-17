d9812698626b58c32f12a3c6920a5472257ef33c
Emil <emil514k@gmai.com>
2019-06-17 04:19:54 +0200

    Brugeradministration controller 1. commit

--- src/main/java/Controllers/BrugeradministrationCTRL.java
+++ src/main/java/Controllers/BrugeradministrationCTRL.java
@@ -6,8 +6,8 @@ import DAL.IDALException;
 
 public class BrugeradministrationCTRL {
 
-    public boolean BrugerIdCheck(int brugerId,IBrugerDAO iBrugerDAO) throws IDALException.DALException {
-        if (iBrugerDAO.getBrugerId(brugerId)!= null) {
+    public boolean BrugerIdCheck(int brugerId,) throws IDALException.DALException {
+        if (iBrugerDAO.getBrugerId(brugerId) == brugerId) { //TODO få IBrugerDAO.getBruger til at virke
             return true;
         }
         else {
@@ -15,15 +15,20 @@ public class BrugeradministrationCTRL {
         }
     }
 
-    public boolean LoginChecker(int brugerId, String brugerPassword,IBrugerDAO iBrugerDAO){
-        if (BrugerIdCheck(brugerId){
+    public boolean LoginChecker(int brugerId, String brugerPassword){
+        if (BrugerIdCheck(brugerId) && iBrugerDAO.getBrugerPassword.equals(brugerPassword)){
+            return true;
+        }else return false;
 
-        }
     }
     public void opretBruger(int brugerId, String brugerNavn, String brugerIni, String brugerPassword) {
 
         if (!BrugerIdCheck(brugerId)){
-            IBrugerDAO.opretBruger(brugerId,brugerNavn,brugerIni,brugerPassword);
+            IBrugerDAO.opretBruger(brugerId,brugerNavn,brugerIni,brugerPassword); //TODO få IBrugerDAO til at virke
         }
+    }
+
+    public void retBruger(int brugerId, String brugerNavn, String brugerIni, String brugerPassword){ //TODO tilføj roller som en variable
+
     }
 }
